6cd8bded15f191df185fe603e513d86330cddecc
sagarsoni67 <121487974+sagarsoni67@users.noreply.github.com>
2023-09-18 20:38:16 +0530

    Update Auth.js

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -8,6 +8,8 @@ const { passwordUpdated } = require("../mail/templates/passwordUpdate")
 const Profile = require("../models/Profile")
 require("dotenv").config()
 
+// Signup Controller for Registering Users
+
 exports.signup = async (req, res) => {
   try {
     // Destructure fields from the request body
